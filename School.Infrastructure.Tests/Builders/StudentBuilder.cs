c80f2df32821c72af60921e2ca6183992fdec5ab
Alexander Shkurlatov <shkurlatov80@gmail.com>
2022-01-23 14:42:40 +1200

    Created sample unit tests for the School.Infrastructure project.

---
+++ School.Infrastructure.Tests/Builders/StudentBuilder.cs
@@ -0,0 +1,32 @@
+﻿using School.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace School.Infrastructure.Tests.Builders
+{
+    public class StudentBuilder
+    {
+        private Student _student;
+        public int TestStudentId => 123;
+        public int TestGroupId => 18;
+        public string TestStudentFirstName => "Test First Name";
+        public string TestStudentLastName => "Test Last Name";
+
+        public StudentBuilder()
+        {
+            _student = WithDefaultValues();
+
+        }
+
+        public Student Build()
+        {
+            return _student;
+        }
+
+        public Student WithDefaultValues()
+        {
+            return Student.Create(TestStudentId, TestGroupId, TestStudentFirstName, TestStudentLastName);
+        }
+    }
+}
---
+++ School.Infrastructure.Tests/Repositories/StudentTests.cs
@@ -0,0 +1,46 @@
+﻿using School.Infrastructure.Data;
+using School.Infrastructure.Repository;
+using School.Infrastructure.Tests.Builders;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace School.Infrastructure.Tests.Repositories
+{
+    public class StudentTests
+    {
+        private readonly SchoolContext _schoolContext;
+        private readonly StudentRepository _studentRepository;
+        private readonly ITestOutputHelper _output;
+        private StudentBuilder StudentBuilder { get; } = new StudentBuilder();
+        private GroupBuilder GroupBuilder { get; } = new GroupBuilder();
+
+        public StudentTests(ITestOutputHelper output)
+        {
+            _output = output;
+            var dbOptions = new DbContextOptionsBuilder<SchoolContext>()
+                .UseInMemoryDatabase(databaseName: "School")
+                .Options;
+            _schoolContext = new SchoolContext(dbOptions);
+            _studentRepository = new StudentRepository(_schoolContext);
+        }
+
+        [Fact]
+        public async Task Get_Student_By_Group()
+        {
+            var existingStudent = StudentBuilder.WithDefaultValues();
+            _schoolContext.Students.Add(existingStudent);
+            var existingGroup = GroupBuilder.WithDefaultValues();
+            _schoolContext.Groups.Add(existingGroup);
+            _schoolContext.SaveChanges();
+
+            var groupId = existingStudent.GroupId;
+            _output.WriteLine($"GroupId: {groupId}");
+            
+            var studentListFromRepo = await _studentRepository.GetStudentByGroupAsync(groupId);
+            Assert.Equal(StudentBuilder.TestGroupId, studentListFromRepo.ToList().First().GroupId);
+        }
+    }
+}
